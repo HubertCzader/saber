26c5067ddb940aa7a3d2249c23e37f32c255d024
Marcin Szubert <marszub@gmail.com>
2023-11-23 12:01:21 +0100

    Refactor Poly17

--- src/Poly17.py
+++ src/Poly17.py
@@ -6,15 +6,16 @@ from src.Polynomial import Polynomial
 
 class Poly17(Polynomial):
     def __init__(self, coefficients):
-        base = ModuloBase(np.array([1, 0, 0, 0, 1], dtype=int), 17, 23)
-        super().__init__(coefficients, base)
+        super().__init__(coefficients, Poly17.get_base())
 
     @staticmethod
     def init_many(multi_coefficients, **kwargs):
-        base = ModuloBase(np.array([1, 0, 0, 0, 1], dtype=int), 17, 23)
-        return Polynomial.init_many(multi_coefficients, base)
+        return Polynomial.init_many(multi_coefficients, Poly17.get_base())
 
     @staticmethod
     def zero(**kwargs):
-        base = ModuloBase(np.array([1, 0, 0, 0, 1], dtype=int), 17, 23)
-        return Polynomial.zero(base)
+        return Polynomial.zero(Poly17.get_base())
+
+    @staticmethod
+    def get_base():
+        return ModuloBase(np.array([1, 0, 0, 0, 1], dtype=int), 17, 23)
