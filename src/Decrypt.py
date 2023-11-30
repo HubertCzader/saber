964dffdfc8e30ffc745dd752710ae11892ae4a0d
HubertCzader <hczader@student.agh.edu.pl>
2023-11-30 12:17:37 +0100

    Small changes

--- src/Decrypt.py
+++ src/Decrypt.py
@@ -1,5 +1,6 @@
 from src.Polynomial import Polynomial
 from src.ModuloBase import ModuloBase
+from src.LogicalShift import shift_right
 
 import numpy as np
 
@@ -10,8 +11,8 @@ def decript(s, cryptogram, epsilon_q, epsilon_p, epsilon_t, n, f, p, q):
     single_coefficient = 2 ** (epsilon_p - 2) - 2 ** (epsilon_p - epsilon_t - 1) + 2 ** (epsilon_q - epsilon_t - 1)
     h2 = Polynomial(np.fill(n, single_coefficient), ModuloBase(f, q))
     v = (b_prim.T @ s)
-    m_prim = (v - 2 ** (epsilon_p - epsilon_t) * c_m + h2).rebase(p)
-
+    m_prim = shift_right((v - 2 ** (epsilon_p - epsilon_t) * c_m + h2).rebase(p))
+    
 
 
 if __name__== "__main__":
--- src/LogicalShift.py
+++ src/LogicalShift.py
@@ -1,7 +1,8 @@
+from src.Polynomial import Polynomial
 import numpy as np
 
 
 # Test function
 def shift_right(vector: np.Array, bits: int):
-    return np.array([np.right_shift(elem, bits) for elem in vector])
+    return np.array([Polynomial(np.right_shift(polynomial.coefficients, bits), polynomial.base) for polynomial in vector])
 
