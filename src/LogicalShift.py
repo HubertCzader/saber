ebdb0891473232ba861cd02f041a1e80e23def98
HubertCzader <hczader@student.agh.edu.pl>
2023-11-30 11:59:18 +0100

    Added possiblity of hampering the value of bits

--- src/LogicalShift.py
+++ src/LogicalShift.py
@@ -2,6 +2,6 @@ import numpy as np
 
 
 # Test function
-def shift_right(vector: np.Array):
-    return np.array([np.right_shift(elem, 1) for elem in vector])
+def shift_right(vector: np.Array, bits: int):
+    return np.array([np.right_shift(elem, bits) for elem in vector])
 
