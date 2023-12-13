bd744e57a62f1ce118de20002b47d9380412125f
Adam Klekowski <adamklekowski@protonmail.com>
2023-12-13 21:16:44 +0100

    add time benchmark

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -8,6 +8,8 @@ End-to-End tests containing:
 - encrypt and decrypt the message
 - compare a decrypted messages against original one.
 """
+import time
+
 import numpy as np
 
 
@@ -16,15 +18,20 @@ def test_end2end():
 
     saber = Saber()
 
-    total_attempts = 10
+    total_attempts = 100_000
     successful_attempts = 0
+    times = []
 
     for _ in range(total_attempts):
-        saber.set_key()
+        start_time = time.time()
+        s, (seed, b) = saber.generate_key()
         msg = np.random.randint(2, size=256)
-        encrypted_msg = saber.encrypt(msg)
-        decrypted_msg = saber.decrypt(encrypted_msg)
-        if decrypted_msg == msg:
+        cryptogram = saber.encrypt(msg, seed, b)
+        decrypted_msg = saber.decrypt(cryptogram, b)
+        end_time = time.time()
+        times.append(end_time-start_time)
+        if np.array_equal(decrypted_msg, msg):
             successful_attempts += 1
     print(f"Successful encryption-decryption attempts ratio: {successful_attempts}/{total_attempts} = "
           f"{successful_attempts/total_attempts:.2f}%.")
+    print(f"Average key generation-encryption-decryption time: {(sum(times) / total_attempts):2f} sec.")
